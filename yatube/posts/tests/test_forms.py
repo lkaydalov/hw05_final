0d84fe68972222ed175cdc6edc060cdcf6095e54
Алексей Кайдалов <lkaydalov@gmail.com>
2022-07-11 19:04:57 +0300

    My 3rd review Final

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -74,7 +74,7 @@ class PostCreateFormTests(TestCase):
         """Проверка полей поста."""
         self.assertEqual(first_object.text, form_data['text'])
         self.assertEqual(
-            first_object.author.username, self.post.author.username
+            first_object.author.username, form_data['author'].username
         )
         self.assertEqual(first_object.group.id, form_data['group'])
         self.assertEqual(
